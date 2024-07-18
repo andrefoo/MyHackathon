3751b53319180f1c72cee622d87a5672b97e3cee
Nicholas Oh <nicholasohjj@gmail.com>
2024-07-19 00:32:16 +0700

    updated packages

--- src/App.js
+++ src/App.js
@@ -1,68 +1,77 @@
-import React, { useEffect, useState, useRef } from 'react';
-import './App.css';
-import VideoCard from './components/VideoCard';
-import BottomNavbar from './components/BottomNavbar';
-import TopNavbar from './components/TopNavbar';
-import SwipeableProductCard from './components/SwipeableProductCard';
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+import VideoCard from "./components/VideoCard";
+import BottomNavbar from "./components/BottomNavbar";
+import TopNavbar from "./components/TopNavbar";
+import SwipeableProductCard from "./components/SwipeableProductCard";
 
 const items = [
   {
-    type: 'video',
-    url: require('./videos/video1.mp4'),
-    profilePic: 'https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-giso/9d429ac49d6d18de6ebd2a3fb1f39269~c5_100x100.jpeg?x-expires=1688479200&x-signature=pjH5pwSS8Sg1dJqbB1GdCLXH6ew%3D',
-    username: 'csjackie',
-    description: 'Lol nvm #compsci #chatgpt #ai #openai #techtok',
-    song: 'Original sound - Famed Flames',
+    type: "video",
+    url: require("./videos/video1.mp4"),
+    profilePic:
+      "https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-giso/9d429ac49d6d18de6ebd2a3fb1f39269~c5_100x100.jpeg?x-expires=1688479200&x-signature=pjH5pwSS8Sg1dJqbB1GdCLXH6ew%3D",
+    username: "csjackie",
+    description: "Lol nvm #compsci #chatgpt #ai #openai #techtok",
+    song: "Original sound - Famed Flames",
     likes: 430,
     comments: 13,
     saves: 23,
     shares: 1,
   },
   {
-    type: 'product',
-    productId: '1',
-    productName: 'Red Leather Handbag',
-    productDescription: 'Crafted with genuine leather. Synthetic lining. Detachable and adjustable shoulder strap. Approx. 20cm*15cm*7cm.',
-    productImage: require('./images/image1.png'),
-    productPrice: '99.00',
+    type: "product",
+    productId: "1",
+    productName: "Red Leather Handbag",
+    productDescription:
+      "Crafted with genuine leather. Synthetic lining. Detachable and adjustable shoulder strap. Approx. 20cm*15cm*7cm.",
+    productImage: require("./images/image1.png"),
+    productPrice: "99.00",
   },
   {
-    type: 'video',
-    url: require('./videos/video2.mp4'),
-    profilePic: 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/eace3ee69abac57c39178451800db9d5~c5_100x100.jpeg?x-expires=1688479200&x-signature=wAkVmwL7lej15%2B16ypSWQOqTP8s%3D',
-    username: 'dailydotdev',
-    description: 'Every developer brain @francesco.ciulla #developerjokes #programming #programminghumor #programmingmemes',
-    song: 'tarawarolin wants you to know this isnt my sound - Chaplain J Rob',
-    likes: '13.4K',
+    type: "video",
+    url: require("./videos/video2.mp4"),
+    profilePic:
+      "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/eace3ee69abac57c39178451800db9d5~c5_100x100.jpeg?x-expires=1688479200&x-signature=wAkVmwL7lej15%2B16ypSWQOqTP8s%3D",
+    username: "dailydotdev",
+    description:
+      "Every developer brain @francesco.ciulla #developerjokes #programming #programminghumor #programmingmemes",
+    song: "tarawarolin wants you to know this isnt my sound - Chaplain J Rob",
+    likes: "13.4K",
     comments: 3121,
     saves: 254,
     shares: 420,
   },
   {
-    type: 'product',
-    productId: '1',
-    productName: 'Red Leather Handbag',
-    productDescription: 'Crafted with genuine leather. Synthetic lining. Detachable and adjustable shoulder strap. Approx. 20cm*15cm*7cm.',
-    productImage: require('./images/image1.png'),
-    productPrice: '99.00',
+    type: "product",
+    productId: "1",
+    productName: "Red Leather Handbag",
+    productDescription:
+      "Crafted with genuine leather. Synthetic lining. Detachable and adjustable shoulder strap. Approx. 20cm*15cm*7cm.",
+    productImage: require("./images/image1.png"),
+    productPrice: "99.00",
   },
   {
-    url: require('./videos/video3.mp4'),
-    profilePic: 'https://p77-sign-va.tiktokcdn.com/tos-maliva-avt-0068/4e6698b235eadcd5d989a665704daf68~c5_100x100.jpeg?x-expires=1688479200&x-signature=wkwHDKfNuIDqIVHNm29%2FRf40R3w%3D',
-    username: 'wojciechtrefon',
-    description: '#programming #softwareengineer #vscode #programmerhumor #programmingmemes',
-    song: 'help so many people are using my sound - Ezra',
+    url: require("./videos/video3.mp4"),
+    profilePic:
+      "https://p77-sign-va.tiktokcdn.com/tos-maliva-avt-0068/4e6698b235eadcd5d989a665704daf68~c5_100x100.jpeg?x-expires=1688479200&x-signature=wkwHDKfNuIDqIVHNm29%2FRf40R3w%3D",
+    username: "wojciechtrefon",
+    description:
+      "#programming #softwareengineer #vscode #programmerhumor #programmingmemes",
+    song: "help so many people are using my sound - Ezra",
     likes: 5438,
     comments: 238,
     saves: 12,
     shares: 117,
   },
   {
-    url: require('./videos/video4.mp4'),
-    profilePic: 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/4bda52cf3ad31c728153859262c329db~c5_100x100.jpeg?x-expires=1688486400&x-signature=ssUbbCpZFJj6uj33D%2BgtcqxMvgQ%3D',
-    username: 'faruktutkus',
-    description: 'Wait for the end | Im RTX 4090 TI | #softwareengineer #softwareengineer #coding #codinglife #codingmemes ',
-    song: 'orijinal ses - Computer Science',
+    url: require("./videos/video4.mp4"),
+    profilePic:
+      "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/4bda52cf3ad31c728153859262c329db~c5_100x100.jpeg?x-expires=1688486400&x-signature=ssUbbCpZFJj6uj33D%2BgtcqxMvgQ%3D",
+    username: "faruktutkus",
+    description:
+      "Wait for the end | Im RTX 4090 TI | #softwareengineer #softwareengineer #coding #codinglife #codingmemes ",
+    song: "orijinal ses - Computer Science",
     likes: 9689,
     comments: 230,
     saves: 1037,
@@ -141,34 +150,36 @@ const items = [
 //       </div>
 //     </div>
 //   );
-  
+
 // }
 
 // export default App;
 
-function App() {
+const App = () => {
   const videoRefs = useRef([]);
 
   useEffect(() => {
     const observerOptions = {
       root: null,
-      rootMargin: '0px',
+      rootMargin: "0px",
       threshold: 0.8,
     };
 
     const handleIntersection = (entries) => {
       entries.forEach((entry) => {
+        const videoElement = entry.target;
         if (entry.isIntersecting) {
-          const videoElement = entry.target;
           videoElement.play();
         } else {
-          const videoElement = entry.target;
           videoElement.pause();
         }
       });
     };
 
-    const observer = new IntersectionObserver(handleIntersection, observerOptions);
+    const observer = new IntersectionObserver(
+      handleIntersection,
+      observerOptions
+    );
 
     videoRefs.current.forEach((videoRef) => {
       if (videoRef) {
@@ -186,13 +197,13 @@ function App() {
   };
 
   const handleSwipeLeft = (item) => {
-    console.log('Swiped left:', item);
-    // Handle swipe left action
+    // console.log("Swiped left:", item);
+    // // Handle swipe left action
   };
 
   const handleSwipeRight = (item) => {
-    console.log('Swiped right:', item);
-    // Handle swipe right action
+    // console.log("Swiped right:", item);
+    // // Handle swipe right action
   };
 
   return (
@@ -200,7 +211,7 @@ function App() {
       <div className="container">
         <TopNavbar className="top-navbar" />
         {items.map((item, index) => {
-          if (item.type === 'video') {
+          if (item.type === "video") {
             return (
               <VideoCard
                 key={index}
@@ -217,7 +228,7 @@ function App() {
                 autoplay={index === 0}
               />
             );
-          } else if (item.type === 'product') {
+          } else if (item.type === "product") {
             return (
               <SwipeableProductCard
                 key={index}
@@ -233,6 +244,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
