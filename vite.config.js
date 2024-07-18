7364d4aa774ff8af1861bc4673f5d418dfdbe2eb
Nicholas Oh <nicholasohjj@gmail.com>
2024-07-19 00:40:08 +0700

    used vite instead

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-// @ts-check
-import reactPlugin from 'vite-plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-/**
- * @type { import('vite').UserConfig }
- */
-const config = {
-  jsx: 'react',
-  plugins: [reactPlugin]
-}
-
-export default config
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+  build: {
+    outDir: 'build',
+  },
+});
