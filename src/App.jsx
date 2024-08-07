792c2a48fd1eeeb26d6c00f7d1dcfe648266f150
Nicholas Oh <nicholasohjj@gmail.com>
2024-08-07 23:37:01 +0800

    removed swipe function from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-import React, { useState } from 'react';
-import { useSwipeable } from 'react-swipeable';
+import React from 'react';
 import './App.css';
 import Home from './home';
-import Product from './product';
 
 const App = () => {
-  const [showHome, setShowHome] = useState(true);
-  const [swipeClass, setSwipeClass] = useState('');
-
-  const handlers = useSwipeable({
-    onSwipedRight: () => {
-      setSwipeClass('swipe-right');
-      setTimeout(() => {
-        setShowHome(false);
-        setSwipeClass('');
-      }, 300); // Duration of the swipe animation
-    }
-  });
-
   return (
-    <div {...handlers} className={`swipe-container ${swipeClass}`}>
-      {showHome ? <Home /> : <Product />}
+    <div className="swipe-container">
+      <Home />
     </div>
   );
 };
